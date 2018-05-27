0f176db220176c30b5fb8c1293dbb75845b585b1
matt dowdney <theofficialmatt@gmail.com>
2018-05-27 15:19:37 -0700

    follow blog building project

---
+++ src/main/java/models/Post.java
@@ -0,0 +1,25 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Post {
+    private final String content;
+    private static ArrayList<Post> instances = new ArrayList<>();
+
+    public Post(String content){
+        this.content = content;
+        instances.add(this);
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public static ArrayList<Post> getAll(){
+        return null;
+    }
+
+    public static void clearAllPosts(){
+        instances.clear();
+    }
+}
